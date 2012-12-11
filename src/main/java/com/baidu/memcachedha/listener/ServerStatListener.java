8dad37450ed9d4de800add15dbc8fb0a265f8cb8
xuchenCN <chenxu198511@gmail.com>
2012-12-11 13:15:36 +0800

    add note

--- src/main/java/com/baidu/memcachedha/listener/ServerStatListener.java
+++ src/main/java/com/baidu/memcachedha/listener/ServerStatListener.java
@@ -4,7 +4,7 @@ import com.baidu.memcachedha.MemcachedClientKeeper;
 
 /**
  * if you want listen server status event instance this class and set it to MemcachedClientDispatch
- * @author chenxu04
+ * @author xuchenCN
  *
  */
 public interface ServerStatListener {
